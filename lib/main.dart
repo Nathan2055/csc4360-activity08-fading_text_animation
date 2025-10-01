28a92ec740dce7f30ac07046b698eb93b68f3d81
Nathan Larsen <nlarsen@nathan2055.com>
2025-09-30 20:47:23 -0400

    Implement light and dark mode switcher in AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,9 @@ class FadingTextAnimation extends StatefulWidget {
 }
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  ThemeMode _themeMode = ThemeMode.light;
+  bool darkMode = false;
+
   bool _isVisible = true;
 
   void toggleVisibility() {
@@ -27,18 +30,45 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Fading Text Animation')),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fading Text Animation'),
+          actions: <Widget>[
+            IconButton(
+              // if dark mode, show sun; if light mode, show moon
+              icon: darkMode
+                  ? const Icon(Icons.sunny)
+                  : const Icon(Icons.mode_night),
+              // same idea for the tooltip text
+              tooltip: darkMode ? 'Light Mode' : 'Dark Mode',
+              onPressed: () {
+                setState(() {
+                  // if on, switch to light mode; if off, switch to dark mode
+                  _themeMode = darkMode ? ThemeMode.light : ThemeMode.dark;
+                  darkMode = !darkMode;
+                });
+              },
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
+        body: Center(
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: Duration(seconds: 1),
+            child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: toggleVisibility,
+          child: Icon(Icons.play_arrow),
+        ),
       ),
     );
   }
